a60cf90b036d1da9926e519c8e7065a3db6b2851
Aurora <jack.burt1@hotmail.co.uk>
2017-07-31 03:18:01 +0100

    Added github and adjusted margins

--- js/index.js
+++ js/index.js
@@ -21,16 +21,23 @@ navBar.on("click" , "a" , function(){
 	if(navBar.find('.active').find('a').attr('id') === "social1") {
 		socialInteract.find('twitter-follow-button').remove();
 		socialInteract.find('.fb-follow').remove();
+		socialInteract.find('.github-button').remove();
 		
 		socialInteract.append('<a href="https://twitter.com/jackburtdev" class="twitter-follow-button" data-show-count="false">Follow @jackburtdev</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>');
 	} else if(navBar.find('.active').find('a').attr('id') === "social2") {
 		socialInteract.find('.twitter-follow-button').remove();
 		socialInteract.find('.fb-follow').remove();
+		socialInteract.find('.github-button').remove();
 
-		
 		socialInteract.append('<div class="fb-follow" data-href="https://www.facebook.com/jack.burt.100" data-layout="button" data-size="small" data-show-faces="false"></div>');
-		console.log("fbworks");
+	} else if(navBar.find('.active').find('a').attr('id') === "social3") {
+		socialInteract.find('.twitter-follow-button').remove();
+		socialInteract.find('.fb-follow').remove();
+		socialInteract.find('.github-button').remove();
+		
+		socialInteract.append('<a class="github-button" href="https://github.com/jackburt12" aria-label="Follow @jackburt12 on GitHub">Follow @jackburt12</a>');
 	}
+
 	
     var navTap = $(this).closest('.navbar');
     var mrkWidth = $(this).parent('li').width();
@@ -63,10 +70,14 @@ $(window).on('scroll',function(){
     if (stop > mainbottom) {
         $('.header').addClass('past-main');
 		$('.social').addClass('past-main');
+		$('.social-interact').addClass('past-main');
+
 
     } else {
         $('.header').removeClass('past-main');
 		$('.social').removeClass('past-main');
+		$('.social-interact').removeClass('past-main');
+
 
 		
    }
