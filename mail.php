02453158786abaa6d0be553fc904be848f7a8bf3
DESKTOP-4QHEGKU\jackb <jack.burt1@hotmail.co.uk>
2017-10-26 14:51:16 +0100

    Another mail attempt, not expecting much change..

--- mail.php
+++ mail.php
@@ -1,30 +1,69 @@
 <?php
+/*
+This first bit sets the email address that you want the form to be submitted to.
+You will need to change this value to a valid email address that you can access.
+*/
+$webmaster_email = "contact@jackburt.me.uk";
 
-if(isset($_POST['name']) && isset($_POST['email'])) {
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$to = 'contact@jackburt.me.uk';
-	$subject = $_POST['subject'];
-	$body = '<html>
-				<body>
-					<p>Name:<br>'.$name.'</p>
-					<p>Email<br>'.$email.'</p>
-				</body>
-			</html>'	
+/*
+This bit sets the URLs of the supporting pages.
+If you change the names of any of the pages, you will need to change the values here.
+*/
+$feedback_page = "feedback_form.html";
+$error_page = "error_message.html";
+$thankyou_page = "thank_you.html";
 
-	$headers = "From: ".$name" <".$email.">\r\n";
-	$headers .= "Reply-To: ".$email."\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-type: text/html; charset=utf-8";
+/*
+This next bit loads the form field data into variables.
+If you add a form field, you will need to add it here.
+*/
+$name = $_REQUEST['name'];
+$subject = $_REQUEST['subject'];
+$email_address = $_REQUEST['email'] ;
+$message = $_REQUEST['message'] ;
 
-	$send = mail($to, $subject, $body, $headers);
-
-	if($send) {
-		echo '<br>';
-		echo 'Thanks for contacting me. I will be with you shortly';
-	} else {
-		echo 'error';
+/*
+The following function checks for email injection.
+Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
+*/
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
 	}
 }
 
+// If the user tries to access this script directly, redirect them to the feedback form,
+if (!isset($_REQUEST['email_address'])) {
+header( "Location: $feedback_page" );
+}
+
+// If the form fields are empty, redirect to the error page.
+elseif (empty($email_address) || empty($message)) {
+header( "Location: $error_page" );
+}
+
+// If email injection is detected, redirect to the error page.
+elseif ( isInjected($email_address) ) {
+header( "Location: $error_page" );
+}
+
+// If we passed all previous tests, send the email then redirect to the thank you page.
+else {
+mail( "$webmaster_email", "$subject",
+  "$message", "From: $email_address" );
+header( "Location: $thankyou_page" );
+}
 ?>
